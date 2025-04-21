4d645ca429e67bf2282d93a49b65c6261d5ccbc1
wiktorswanson <wiktor.swanson@gmail.com>
2025-04-21 15:04:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import asyncio
 import json
 from fastapi import FastAPI, WebSocket
-from fastapi.responses import HTMLResponse
 import websockets
-import uvicorn
 
 app = FastAPI()
-
 clients = set()
 
 @app.websocket("/ws")
@@ -16,24 +13,22 @@ async def websocket_endpoint(websocket: WebSocket):
     try:
         while True:
             await asyncio.sleep(1)
-    except Exception:
+    except:
         clients.remove(websocket)
 
 async def stream_ais_data():
     url = "wss://stream.aisstream.io/v0/stream"
     API_KEY = "940bf3259b0a8f7c3e1e4fb317b37bba918072fa"
-
-    async with websockets.connect(url) as websocket:
+    async with websockets.connect(url) as ws:
         auth_msg = {
             "APIKey": API_KEY,
             "BoundingBoxes": [
                 {"MinLat": 55.0, "MinLon": 12.0, "MaxLat": 60.0, "MaxLon": 18.0}
             ]
         }
-        await websocket.send(json.dumps(auth_msg))
-
+        await ws.send(json.dumps(auth_msg))
         while True:
-            msg = await websocket.recv()
+            msg = await ws.recv()
             for client in clients:
                 try:
                     await client.send_text(msg)
@@ -43,4 +38,3 @@ async def stream_ais_data():
 @app.on_event("startup")
 async def startup_event():
     asyncio.create_task(stream_ais_data())
-
